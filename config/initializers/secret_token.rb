d05ae98b27fff4e503c2bf2598080516a07d41d0
skelly <skelly@debo.thetemplehost.com>
2012-11-25 16:55:24 -0500

    Initial Commit - GIT LEARNING GO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTest1::Application.config.secret_token = '6b72d4352d9f260712faa7efabca22fcd671a5ede61e7e9a383c18b2c3a41855b5c6df6888f83d6a5ef7d846bfa7c149ac3b2d7aa20d55138325deb74e9f8aed'
